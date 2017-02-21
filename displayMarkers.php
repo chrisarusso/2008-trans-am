c6abe68734a1d7b6870270edfb0deee59e1b5e76
Chris Russo <chris.andrews.russo@gmail.com>
2017-02-20 19:28:42 -0500

    Pull from CSV rather than MySQL query

--- displayMarkers.php
+++ displayMarkers.php
@@ -1,31 +1,22 @@
 <?php
-$conn = mysql_connect("mysql.wikicommunity.org","root2", 'r00tb33r');
-$db = mysql_select_db("track3r");
-	
-$query =  'select lat ,  lon, time2  from tracking2 where (id % 30 = 0) 
-or (id 
-= 1) or (id % 5 = 0 and substring(time2,1,5) = (select 
-max(substring(time2,1,5)) from 
-tracking2) or id = (select max(id) from tracking2))';
-$res = mysql_query($query);
+
+/*
+ * Previous implementation relied on MySQL query
+ * We've now flattened things to a CSV file we read from
+ */
+$csv = array_map('str_getcsv', file('wc-outfile.csv'));
 
 
-		while($row = mysql_fetch_object($res)){
-			$arr[] = array($row->lat, $row->lon, $row->time2);
-		}
-		
-		$mostRecent = $arr[count($arr)-1][2];
+// Output XML that the map JS will read from.
+// Perhaps not well formed?
 
-		
-		
-		echo '<?xml version="1.0" encoding="ISO-8859-1"?>
-				<markers>';
+	echo
+  '<?xml version="1.0" encoding="ISO-8859-1"?>
+    <markers>';
+    if(is_array($csv)){
+      foreach($csv as $key => $val){
+        echo "<marker time=\"{$val[4]}\" lat=\"{$val[1]}\" lon=\"{$val[2]}\" />\n";
+      }
+    }
 
-        if(is_array($arr)){
-		foreach($arr as $key => $val){
-				echo "<marker time=\"{$val[2]}\" lat=\"{$val[0]}\" lon=\"{$val[1]}\" />\n";
-			
-		}
-        }
-		
-		echo "</markers>";
+  echo "</markers>";
